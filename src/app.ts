cf55afd3974000b3db00c82154fc75e53089c355
wangyong220531 <wangyong220531@gmail.com>
2023-09-11 14:27:35 +0800

    2023091114:27update

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,32 @@
-import { PropsWithChildren } from 'react'
-import { useLaunch } from '@tarojs/taro'
-import './app.less'
+import { PropsWithChildren } from "react";
+import Taro, { useLaunch } from "@tarojs/taro";
+import "./app.less";
 
 function App({ children }: PropsWithChildren<any>) {
-
   useLaunch(() => {
-    console.log('App launched.')
-  })
+    console.log("App launched.");
+    Taro.cloud.init({
+      env: "geshu-bidding-5gnhpdzpb49a69a4",
+    });
+
+    Taro.cloud.callFunction({
+      name: "getOpenId",
+      data: {
+        // 传递给云函数的参数
+      },
+      success: (res) => {
+        // 处理云函数返回的结果
+        console.log(1, res);
+      },
+      fail: (err) => {
+        // 处理调用云函数过程中的错误
+        console.log(2, err.errMsg);
+      },
+    });
+  });
 
   // children 是将要会渲染的页面
-  return children
+  return children;
 }
 
-export default App
+export default App;
