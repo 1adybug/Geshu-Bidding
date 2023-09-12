89736893bbe14ca7f64cfd99dea0efdf658defdd
wangyong220531 <wangyong220531@gmail.com>
2023-09-12 11:26:10 +0800

    202309121126update

--- cloudfunctions/myFunction/index.js
+++ cloudfunctions/myFunction/index.js
@@ -1,10 +1,13 @@
+// eslint-disable-next-line import/no-commonjs
 const axios = require("axios");
+// eslint-disable-next-line import/no-commonjs
 const cloud = require("wx-server-sdk");
 
 cloud.init({
   env: cloud.DYNAMIC_CURRENT_ENV,
 });
 
+// eslint-disable-next-line import/no-commonjs
 exports.main = async () => {
   try {
     const response = await axios.get(
--- src/constant/functions.ts
+++ src/constant/functions.ts
@@ -1,1 +1,6 @@
-export const Function_MyFunction = "myFunction";
+export const Function_BidRejectionOrTermination = "";
+export const Function_Contract = "";
+export const Function_Correct = "";
+export const Function_PurchaseIntention = "";
+export const Function_PurchaseSolicitation = "";
+export const Function_ResultsOrShortlisted = "";
