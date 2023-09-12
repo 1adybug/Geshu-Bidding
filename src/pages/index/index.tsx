5e1fc75efd5d50eb778a29b8742b2db51f5d98d2
wangyong220531 <wangyong220531@gmail.com>
2023-09-12 08:19:52 +0800

    202309120819update

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -12,9 +12,53 @@ export default function Index() {
 
   const [drawShow, setDrawShow] = useState(false)
 
+  function extractUlContents(html) {
+    var pattern = /<ul\b[^>]*>([\s\S]*?)<\/ul>/g;
+    var match;
+    var ul_contents = [];
+  
+    while ((match = pattern.exec(html)) !== null) {
+      var ul_content = match[1];
+      ul_contents.push(ul_content);
+    }
+  
+    if (ul_contents.length > 0) {
+      ul_contents.forEach(function (content) {
+        console.log(extractDataFromHTML(content));
+        // console.log("---"); // Print a separator line for different <ul> contents
+      });
+    } else {
+      console.log("No content found within any <ul> tags.");
+    }
+  }
+  
+  function extractDataFromHTML(html) {
+    const regex =
+      /<span class="lb-time">(.*?)<\/span>\s*<a href="(.*?)" target="_blank" title="(.*?)">/g;
+  
+    const results = [];
+    let match;
+    while ((match = regex.exec(html)) !== null) {
+      const spanContent = match[1];
+      const href = match[2];
+      const title = match[3];
+  
+      const obj = {
+        time: spanContent,
+        href: href,
+        title: title,
+      };
+  
+      results.push(obj);
+    }
+  
+    return results;
+  }
+  
+
   useEffect(() => {
     fetchTest().then((res) => {
-      console.log(1,res);
+      console.log(1, extractUlContents(res.result));
     }).catch(err => {
       console.log(2, err);
     })
